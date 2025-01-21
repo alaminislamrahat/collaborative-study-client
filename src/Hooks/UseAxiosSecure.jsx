b013cbb81d9ce0aa0d9cd26b0ffaea8e86ccedaf
unknown <alamin>
2025-01-21 18:25:34 +0600

    interceptor added

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,12 +1,40 @@
 import axios from "axios";
 
+import { useNavigate } from "react-router-dom";
+import UseAuth from "./useAuth";
 
-const AxiosSecure = axios.create({
-    baseURL : 'http://localhost:5000'
+
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000'
 })
+const useAxiosSecure = () => {
+    const { logOut } = UseAuth();
+    const navigate = useNavigate()
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        // console.log('iterceptor', token)
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        return Promise.reject(error)
+    })
 
-const UseAxiosSecure = () => {
-    return AxiosSecure;
+
+    // response interceptors 
+    axiosSecure.interceptors.response.use(function (response) {
+
+        return response;
+    }, async function (error) {
+        const status = error.response.status;
+        // console.log('in the interceptor error', status)
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    });
+    return axiosSecure;
 };
 
-export default UseAxiosSecure;
+export default useAxiosSecure;
